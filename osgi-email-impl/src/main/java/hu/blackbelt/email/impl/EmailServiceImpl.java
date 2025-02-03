4b178204ce7883b3efb9d773b3239957b66f93a9
Robert Csakany <csakany.robert@gmail.com>
2025-02-03 15:32:39 +0100

    Transfer

--- osgi-email-impl/src/main/java/hu/blackbelt/email/impl/EmailServiceImpl.java
+++ osgi-email-impl/src/main/java/hu/blackbelt/email/impl/EmailServiceImpl.java
@@ -36,8 +36,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
-import javax.mail.Transport;
-import javax.mail.internet.MimeMessage;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.MimeMessage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
